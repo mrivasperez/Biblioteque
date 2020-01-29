03bef15472df58ec40613d510f52c3919d53680f
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-01-28 21:36:42 -0800

    updates to styling

--- assets/app.js
+++ assets/app.js
@@ -37,25 +37,51 @@ class UI {
         //insert tr tag
         const row = document.createElement('tr');
 
-        //add html to row
+        // add html to row
         row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-        
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-outline-danger btn-sm delete">X</a></td>
         `;
+        // append the row to the list
+        list.appendChild(row);
 
     };
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    };
 
-
-}
+};
 
 
 // STORAGE
 
 // DISPLAY BOOKS
 
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
 //ADD A BOOk
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    //prevent default action (submit form - refreshes webpage!)
+    e.preventDefault();
+
+    //get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // instantiate book
+    const book = new Book(title, author, isbn);
+
+    // add book to the ui
+    UI.addBookToList(book);
+
+    // clear fields after submit
+    UI.clearFields();
+
+})
 
 //REMOVE A BOOK
